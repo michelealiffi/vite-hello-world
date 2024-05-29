09c523c8e7096e29b970d7a1ddfb989700714abe
michelealiffi <michelealiffi890@gmail.com>
2024-05-29 14:50:19 +0200

    Finish + bonus

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Vue - Official](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (previously Volar) and disable Vetur
+
+## Exercise: Vite Hello World
+
+Create un nuovo progetto utilizzando Vite: aiutatevi con le slide per ripercorrere i vari passaggi dell'installazione come visti a lezione.
+Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita "La mia prima app con Vite!"
+Bonus:
+Creare un secondo componente AppHero, che visualizza un jumbotron.
---
+++ src/components/AppHero.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'AppHero',
+  data() {
+    return {
+        imgName: 'img01.png',
+    };
+  },
+
+  methods: {
+    getImgUrl(img) {
+        return new URL(`../assets/${img}`, import.meta.url).href;
+    },
+  },
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <div class="container">
+            <div class="left">
+                <h2>Jumbotron</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eveniet. Reprehenderit, magni rerum.
+                    Fuga, itaque? Aspernatur, ad doloribus quod eveniet quae at voluptatem omnis, non officia libero
+                    corrupti similique nostrum?</p>
+                <img src="../assets/img01.png" alt="" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
